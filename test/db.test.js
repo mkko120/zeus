3884737e4796a06e91a995c471f50f09f9e2ec69
mkko120 <mrmikillll@gmail.com>
2022-07-02 14:48:04 +0200

    db.test.js

--- test/db.test.js
+++ test/db.test.js
@@ -4,41 +4,38 @@ const expect = require("chai").expect;
 
 describe('Database', () => {
     describe('Setting Values', () => {
-        it('should set the values to database', () => {
-            db.set('foo', 'bar');
+        it('should set the values to database', async () => {
+            await db.set('foo', 'bar');
         });
-        it('should get { foo: bar } from database', () => {
-            setTimeout(async () => {
-                let result = await db.get('foo');
-                expect(result).to.equal('bar');
-            }, 2000)
+        it('should get { foo: bar } from database', async () => {
+            let result = await db.get('foo');
+            expect(result).to.equal('bar');
         });
     });
     describe('Replacement', () => {
-        it('should replace values with new values', () => {
-            db.set('foo', 'rab');
+        it('should replace values with new values', async () => {
+            await db.set('foo', 'rab');
         });
-        it('should get { foo: rab } from database', () => {
-            setTimeout(async () => {
-                const result = await db.get('foo');
-                expect(result).to.equal('rab');
-            }, 2000)
-
+        it('should get { foo: rab } from database', async () => {
+            const result = await db.get('foo');
+            expect(result).to.equal('rab');
         });
-        it('should not get { foo: ger } from database', () => {
-            setTimeout(async () => {
-                const result = await db.get('foo');
-                expect(result).to.not.equal('ger');
-            }, 2000)
+        it('should not get { foo: ger } from database', async () => {
+            const result = await db.get('foo');
+            expect(result).to.not.equal('ger');
         })
     });
     describe('Clearing database', () => {
         it('should clear the Database', async () => {
             await db.purge();
         });
-        it('should not have any values in Database', async () => {
+        it('should not have any values in Database [get]', async () => {
+            const result = await db.get('foo');
+            expect(result).to.be.null;
+        });
+        it('should not have any values in Database [getAll]', async () => {
             const result = await db.getAll();
-            expect(result.foo).to.equal(undefined);
+            expect(result.foo).to.be.undefined;
         });
     });
 });
